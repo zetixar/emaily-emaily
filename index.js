078c4553745bf8d62f11374daa2162b13e154e59
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-08-29 07:39:59 -0400

    Add body-parser to express to have req.body in post requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 const passport = require('passport')
 const keys = require('./config/keys')
 
@@ -10,6 +11,8 @@ require('./services/passport')
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(bodyParser.json())
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
